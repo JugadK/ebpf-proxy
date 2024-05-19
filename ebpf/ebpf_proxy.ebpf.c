60eb1679a4e370cda94a697e5b3239fc878ec6c5
Jugad Khajuria <jugad@outlook.com>
2024-05-19 02:51:36 -0700

    update ebpf code too route reigstered packets

--- ebpf/ebpf_proxy.ebpf.c
+++ ebpf/ebpf_proxy.ebpf.c
@@ -1,46 +1,95 @@
-#include <linux/types.h>
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <linux/in.h>
 #include <bpf/bpf_endian.h>
+#include <arpa/inet.h>
+#include <linux/bpf.h>
 #include <linux/if_ether.h>
+#include <linux/in.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/types.h>
+#include <bpf/bpf_helpers.h>
+#include <netinet/in.h>
+
+#define PROXY_IP 0x00000000;
 
 char _license[] SEC("license") = "GPL";
 
-struct {
-  __uint(type, BPF_MAP_TYPE_ARRAY);
-  __type(key, __u32);
-  __type(value, __u32);
-  __uint(max_entries, 4);
-} ipv4addr SEC(".maps");
+struct ipv4_lpm_key {
+        __u32 prefixlen;
+        __u32 data;
+};
 
+struct {
+  __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+  __type(key, struct ipv4_lpm_key);
+  __type(value, __u32);
+  __uint(max_entries, 0xFFFF);
+  __uint(map_flags, BPF_F_NO_PREALLOC);
+} src2destipv4 SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+  __type(key, struct ipv4_lpm_key);
+  __type(value, __u32);
+  __uint(max_entries, 0xFFFF);
+  __uint(map_flags, BPF_F_NO_PREALLOC);
+} dest2srcipv4 SEC(".maps");
 
 SEC("xdp")
 int xdp_pass_prog(struct xdp_md *ctx) {
 
   void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
+  void *data = (void *)(long)ctx->data;
   int size = data_end - data;
 
   struct ethhdr *eth = data;
-  struct iphdr *iph = data + sizeof(*eth);
 
-  __u32 key = 0x1;
-  __u32 *ret;
+  if (data+sizeof(struct ethhdr) > data_end)
+    return XDP_PASS; 
+  
+	if (eth->h_proto == bpf_htons(ETH_P_IP)) {
+    
+		struct iphdr *iph = (data + sizeof(struct ethhdr));
 
-  ret = bpf_map_lookup_elem(&ipv4addr, &key);
-
-  if(ret) {
-    bpf_printk("%x", *ret);
-  } else {
-    bpf_printk("Look up failed for key %x", key);
-  }
-
-  __u32 value = 0x4;
-
-  //__u32 rv = bpf_map_update_elem(&ipv4addr, &key, &value, BPF_ANY);
+		if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
+			return XDP_PASS; 
 
 
+    if(iph->protocol == IPPROTO_TCP && data+sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr) > data_end) {
+      
+      struct tcphdr *tcph = data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr);
+
+      if(tcph->dest == 7878) {
+        return XDP_PASS;
+      }
+    }
+
+		__u32 saddr = bpf_ntohl(iph->saddr);
+		__u32 daddr = bpf_ntohl(iph->daddr);
+		
+		bpf_printk("Source: %x Dest: %x", saddr, daddr);
+
+		__u32 *ret;
+
+    struct ipv4_lpm_key ipv4_key = {
+            .prefixlen = 0x20, // Full IP Address
+            .data = saddr,
+    };
+
+		ret = bpf_map_lookup_elem(&src2destipv4, &ipv4_key);
+
+		if (ret) {
+			bpf_printk("Looked up in bpf map source addr, dest addr is %x", *ret);
+
+      __u32 proxy_ip = PROXY_IP;
+
+      iph->saddr = bpf_htons(proxy_ip);
+      iph->daddr = bpf_htons(*ret);
+
+      return XDP_TX;
+		} else {
+    }
+	}
 
   return XDP_PASS;
 }
+
